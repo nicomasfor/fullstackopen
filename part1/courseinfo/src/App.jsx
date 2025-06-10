c6858a8dc22f61cc92126a9eabb69dd5519a537f
Nicolas M <nicomasforner@gmail.com>
2025-06-10 16:32:04 +0200

    courseinfo 1.2
    courseinfo 1.2 backup

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,13 @@
+const Part = (part) => {
+  return(
+    <div>
+      <p>
+        {part.name} {part.num}
+      </p>
+    </div>    
+  )
+}
+
 const Header = (title) => {
   return(
     <div>
@@ -8,12 +18,12 @@ const Header = (title) => {
   )
 }
 
-const Content = (content) => {
+const Content = ({content}) => {
   return(
     <div>
-      <p>
-        {content.name} {content.num}
-      </p>
+      <Part name={content[0].name} num={content[0].num}/>
+      <Part name={content[1].name} num={content[1].num}/>
+      <Part name={content[2].name} num={content[2].num}/>
     </div>    
   )
 }
@@ -37,12 +47,16 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const content = [
+    {name: part1, num:exercises1},
+    {name: part2, num:exercises2},
+    {name: part3, num:exercises3}
+  ]
+
   return (
     <div>
       <Header name={course}/>
-      <Content name={part1} num={exercises1}/>
-      <Content name={part2} num={exercises2}/>
-      <Content name={part3} num={exercises3}/>
+      <Content content={content}/>
       <Total num={exercises1 + exercises2 + exercises3}/>
     </div>
   )
