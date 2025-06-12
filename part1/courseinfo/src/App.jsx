3b42c7ee71dfb82e0d2dfc19c9bd0e3421aa4d92
Nicolas M <nicomasforner@gmail.com>
2025-06-12 21:33:35 +0200

    courseinfo 1.3
    courseinfo 1.3 backup

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -40,24 +40,28 @@ const Total = (total) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const content = [
-    {name: part1, num:exercises1},
-    {name: part2, num:exercises2},
-    {name: part3, num:exercises3}
+    part1, part2, part3
   ]
 
   return (
     <div>
       <Header name={course}/>
       <Content content={content}/>
-      <Total num={exercises1 + exercises2 + exercises3}/>
+      <Total num={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
